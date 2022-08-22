28cf4f775d9c135ddac12c644aff4cebf0760a38
henriquekutnercordeiro <CIANDT\hkutner@lnb034246spo.ciandt.global>
2022-08-22 11:26:18 -0300

    feat: escrevendo cenarios de teste

--- src/main/java/armazem/Armazem.java
+++ src/main/java/armazem/Armazem.java
@@ -26,18 +26,58 @@ public class Armazem {
         }
     }
 
+    public void adicionarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade){
+        if(!estoque.containsKey(ingrediente)) {
+            throw new IllegalArgumentException("Ingrediente não encontrado");
+        } else if(quantidade > 0){
+            int totalUpdated = estoque.get(ingrediente) + quantidade;
+            estoque.put(ingrediente, totalUpdated);
+        } else {
+            throw new IllegalArgumentException("Quantidade invalida");
+        }
+    }
+
+    public void reduzirQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade){
+        int storeQuant = estoque.get(ingrediente);
+        if(!estoque.containsKey(ingrediente)){
+            throw new IllegalArgumentException("Ingrediente não encontrado");
+        } else if(quantidade <= 0){
+            throw new IllegalArgumentException("Quantidade invalida");
+        } else if(storeQuant == quantidade || storeQuant < quantidade) {
+            estoque.remove(ingrediente);
+            System.out.println("Ingrediente removido");
+        } else {
+            int totalUpdated = storeQuant - quantidade;
+            estoque.put(ingrediente, totalUpdated);
+        }
+
+    }
+
+    public Integer consultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente){
+        if(!estoque.containsKey(ingrediente)){
+            throw new IllegalArgumentException("Ingrediente não encontrado");
+        } else {
+            return estoque.get(ingrediente);
+        }
+    }
+
+
+    // Getters
     public TreeMap<Ingrediente, Integer> getEstoque() {
         return estoque;
     }
 
     public static void main(String[] args) {
 
-        Armazem ss = new Armazem();
+        Armazem myStore = new Armazem();
         Base sorvete = new Base(TipoBase.SORVETE);
+        myStore.cadastrarIngredienteEmEstoque(sorvete);
 
+        myStore.adicionarQuantidadeDoIngredienteEmEstoque(sorvete, 5);
+        myStore.reduzirQuantidadeDoIngredienteEmEstoque(sorvete, 3);
+        System.out.println(myStore.getEstoque());
+        System.out.println(myStore.consultarQuantidadeDoIngredienteEmEstoque(sorvete));
 
-        ss.cadastrarIngredienteEmEstoque(sorvete);
-        System.out.println(ss.getEstoque().get(sorvete));
 
     }
 
--- src/test/java/armazem/ArmazemTest.java
+++ src/test/java/armazem/ArmazemTest.java
@@ -3,12 +3,14 @@ package armazem;
 import ingredientes.base.Base;
 import ingredientes.base.TipoBase;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.InvalidClassException;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArmazemTest {
@@ -21,9 +23,23 @@ public class ArmazemTest {
     }
 
     @Test
-    public void should_add__properly() {
+    public void should_add_ingredient() {
         Base sorvete = new Base(TipoBase.SORVETE);
         armazem.cadastrarIngredienteEmEstoque(sorvete);
         assertEquals(1, armazem.getEstoque().size());
     }
+
+    @Test
+    public void should_dispatch_exception_when_ingredient_not_found() {
+        Base sorvete = new Base(TipoBase.SORVETE);
+        armazem.cadastrarIngredienteEmEstoque(sorvete);
+        armazem.descadastrarIngredienteEmEstoque(sorvete);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            armazem.descadastrarIngredienteEmEstoque(sorvete);
+        });
+        String expectedMessage = "Ingrediente não encontrado";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
